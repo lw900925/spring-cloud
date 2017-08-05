a3a8e5e4ef332d9fb2ca5a0b413fededeaf63552
一书生VOID <lw900925@163.com>
2017-08-05 16:39:51 +0800

    Reinitialized projects

--- springcloud-config-server/src/main/java/org/matrixstudio/springcloud/config/ConfigServerApplication.java
+++ springcloud-config-server/src/main/java/org/matrixstudio/springcloud/config/ConfigServerApplication.java
@@ -2,15 +2,13 @@ package org.matrixstudio.springcloud.config;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.config.server.EnableConfigServer;
 
 @SpringBootApplication
-@EnableDiscoveryClient
 @EnableConfigServer
-public class ServerApplication {
+public class ConfigServerApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(ServerApplication.class, args);
+        SpringApplication.run(ConfigServerApplication.class, args);
     }
 }
