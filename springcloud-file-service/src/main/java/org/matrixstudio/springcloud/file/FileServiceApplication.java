add8a845d6605100427031917554202c154bdabb
一书生VOID <lw900925@163.com>
2017-08-09 15:56:10 +0800

    新增springcloud-file-service服务

---
+++ springcloud-file-service/src/main/java/org/matrixstudio/springcloud/file/FileServiceApplication.java
@@ -0,0 +1,12 @@
+package org.matrixstudio.springcloud.file;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FileServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FileServiceApplication.class, args);
+    }
+}
