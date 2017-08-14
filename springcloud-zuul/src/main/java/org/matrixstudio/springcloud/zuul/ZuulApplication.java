c71999b8868556f25a879f26bdad7372dd993b79
lw900925 <lw900925@163.com>
2017-08-14 11:59:33 +0800

    新增springcloud-zuul模块

---
+++ springcloud-zuul/src/main/java/org/matrixstudio/springcloud/zuul/ZuulApplication.java
@@ -0,0 +1,14 @@
+package org.matrixstudio.springcloud.zuul;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class ZuulApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZuulApplication.class, args);
+    }
+}
